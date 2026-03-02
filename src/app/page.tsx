ecfa768d45b907967cee0c5c3cd028398dba09d9
TheCodeFather0 <raminmammadov056@gmail.com>
2026-03-02 20:53:19 +0400

    created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+import { IUser } from "@/types/home-page-types";
+
 const App = () => {
+  const users: IUser[] = [
+    {
+      age: 12,
+      name: "ramin",
+      surname: "mammadzada",
+      langs: ["html", "css"],
+      getFullName() {
+        return this.name + " " + this.surname;
+      },
+    },
+    {
+      age: 15,
+      name: "leyla",
+      surname: "aliyeva",
+      getFullName() {
+        return this.name + " " + this.surname;
+      },
+    },
+  ];
+
+  type TFunc = (name: string, age: number) => string | number;
+
+  const handleClick: TFunc = (name, age) => {
+    return name + " " + age;
+  };
+
+  handleClick("ramin", 12);
   return (
     <div>
       <h1>Hello world!</h1>
+
+      {users.map((item: IUser) => {
+        return <div key={item.name}>{item.getFullName()}</div>;
+      })}
     </div>
   );
 };
